d867ce2b853d7bf5d6b93329c64d63fa7549514a
Abisha Dhakal <dhakalabisha99@gmail.com>
2026-05-12 08:14:48 +0545

    week 2

--- Week2/operator.js
+++ Week2/operator.js
@@ -1,30 +1,36 @@
-function add() {
+function calculate(operation) {
 
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
-    let sum = num1 + num2;
-    document.getElementById("result").innerHTML =
-        "Addition: " + sum;
-}
-function subtract() {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
-    let difference = num1 - num2;
 
-    document.getElementById("result").innerHTML =
-        "Subtraction: " + difference;
-}
-function multiply() {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
-    let product = num1 * num2;
-    document.getElementById("result").innerHTML =
-        "Multiplication: " + product;
-}
-function divide() {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
-    let division = num1 / num2;
-    document.getElementById("result").innerHTML =
-        "Division: " + division;
+    let result;
+
+    if (operation == "add") {
+        result = num1 + num2;
+        document.getElementById("result").innerHTML =
+            "Addition: " + result;
+    }
+
+    else if (operation == "subtract") {
+        result = num1 - num2;
+        document.getElementById("result").innerHTML =
+            "Subtraction: " + result;
+    }
+
+    else if (operation == "multiply") {
+        result = num1 * num2;
+        document.getElementById("result").innerHTML =
+            "Multiplication: " + result;
+    }
+
+    else if (operation == "divide") {
+        result = num1 / num2;
+        document.getElementById("result").innerHTML =
+            "Division: " + result;
+    }
+
+    else {
+        document.getElementById("result").innerHTML =
+            "Invalid Operation";
+    }
 }
